57932e267482ddcc043ccfaed14baee35c789881
balaji <balajiv.bsc23@rvu.edu.in>
2024-01-29 19:53:56 +0530

    Your commit message

---
+++ problem01.c
@@ -0,0 +1,9 @@
+// 1. Write a C program to print your name.
+
+#include<stdio.h>
+int main()
+{
+    printf("balaji");
+    return 0; 
+
+}
---
+++ problem03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>4
+
+int input();
+int add(int a, int b,int sum);
+void output(int a, int b, int sum);
+int main()
+{
+    int a,b,sum;
+    a=input();
+    b=input();
+    sum=add(a,b,sum);
+    output(a,b,sum);
+    return 0;
+}
+int input()
+{
+int num;
+printf("enter the number\n");
+scanf("%d",&num);
+return num;
+}
+
+int add(int a, int b,int sum)
+{
+sum=a+b;
+return sum;
+}
+void output(int a, int b, int sum)
+{
+    printf("the sum is %d\n",sum);
+}
